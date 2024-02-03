2dabb17b5bd971faf236985b1bdf886eaee2b8a3
Samuel França <samuelhenriques2000@gmail.com>
2024-02-02 22:03:54 -0300

    Install tailwindcss and change initial files

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped>
